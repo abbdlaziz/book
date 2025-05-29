18e9ce8128e1f662c7eefef24af1a31ae9f2f770
az <abdl.alaziz95@gmail.com>
2025-05-30 05:49:46 +0700

    feature : library management

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://47.236.246.176:8080',
+    baseURL: 'https://47.236.246.176',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
